54da590dad6542af301d300d55208621d782032c
khalid017 <68370124+khalid017@users.noreply.github.com>
2022-05-11 19:22:59 +0530

    security update

--- pass_generator/settings.py
+++ pass_generator/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bs!r_50&ppjk9o03(1k)!u=6^wp7s501^f)1#2u8+w(j+w+awz'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
